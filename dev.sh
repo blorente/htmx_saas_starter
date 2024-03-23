dfc5af344e2214bbdfab7a29d6404ce1c4c8ff8f
Borja Lorente <blorente.me@gmail.com>
2024-03-23 17:58:45 +0000

    fix(dev): Clean ports before starting

--- dev.sh
+++ dev.sh
@@ -1,19 +1,23 @@
-air -c air.toml &
-
-AIR_PID=$!
+clean_port() {
+    main_pid=$(lsof -i tcp:9999 | tail -n 1 | awk '{print $2}')
+    kill -2 "${main_pid}" || true
+}
 
 cleanup() {
     kill -2 $AIR_PID || true
-    main_pid=$(lsof -i tcp:9999 | tail -n 1 | awk '{print $2}')
-    kill -2 "${main_pid}" || true
+    clean_port
     exit 0
 }
 
-trap cleanup EXIT SIGINT SIGTERM
-
+air -c air.toml &
+AIR_PID=$!
 npx tailwindcss \
   -i 'styles.css' \
   -o 'public/styles.css' \
   --watch &
+
+trap cleanup EXIT SIGINT SIGTERM
+
+clean_port
 
 wait $AIR_PID
